5f9124a6d9ae3fbf6ff72dd5718bbe18320edd7f
Larissa <larissastarosky@gmail.com>
2023-01-15 11:05:43 -0300

    criar entidade Usuario

---
+++ src/main/java/br/com/springboot/crud/model/Usuario.java
@@ -0,0 +1,51 @@
+package br.com.springboot.crud.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_usuario")
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "nome")
+	private String nome;
+
+	@Column(name = "idade")
+	private Integer idade;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+}
