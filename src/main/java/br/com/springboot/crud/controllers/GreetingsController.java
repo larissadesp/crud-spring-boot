682612154a491502d42503142d39a76d90810dba
Larissa <larissastarosky@gmail.com>
2023-01-15 20:52:10 -0300

    crud e filtros + correção sequence

--- src/main/java/br/com/springboot/crud/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/crud/controllers/GreetingsController.java
@@ -1,26 +1,72 @@
 package br.com.springboot.crud.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- *
- * A sample greetings controller to return greeting text
- */
+import br.com.springboot.crud.model.Usuario;
+import br.com.springboot.crud.repository.UsuarioRepository;
+
 @RestController
 public class GreetingsController {
-    /**
-     *
-     * @param name the name to greet
-     * @return greeting text
-     */
-    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    public String greetingText(@PathVariable String name) {
-        return "Hello " + name + "!";
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+    @PostMapping(value = "salvar")
+    @ResponseBody
+    public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario) {
+    	Usuario novoUsuario = usuarioRepository.save(usuario);
+    	return new ResponseEntity<Usuario>(novoUsuario, HttpStatus.CREATED);
     }
+    
+    @DeleteMapping(value = "deletar")
+    @ResponseBody
+    public ResponseEntity<String> deletar(@RequestParam Long idUsuario) {
+    	usuarioRepository.deleteById(idUsuario);
+    	return new ResponseEntity<String>("Usuário deletado com sucesso!", HttpStatus.OK);
+    }
+    
+    @GetMapping(value = "consultar")
+    @ResponseBody
+    public ResponseEntity<Usuario> consultar(@RequestParam(name = "idUsuario") Long idUsuario) {
+    	Usuario usuario = usuarioRepository.findById(idUsuario).get();
+    	return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+    }
+    
+    @GetMapping(value = "listarTodos")
+    @ResponseBody
+	public ResponseEntity<List<Usuario>> listarTodos() {
+    	List<Usuario> usuarios = usuarioRepository.findAll();
+    	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+    }
+    
+    @PutMapping(value = "atualizar")
+    @ResponseBody
+    public ResponseEntity<?> atualizar(@RequestBody Usuario usuario) {
+    	if (usuario.getId() == null) {
+    		return new ResponseEntity<String>("É necessário informar o id do usuário para atualizar.", HttpStatus.OK);
+    	}
+    	
+    	Usuario usuarioAtualizado = usuarioRepository.saveAndFlush(usuario);
+    	return new ResponseEntity<Usuario>(usuarioAtualizado, HttpStatus.OK);
+    }
+    
+    @GetMapping(value = "pesquisarPorNome")
+    @ResponseBody
+    public ResponseEntity<List<Usuario>> pesquisarPorNome(@RequestParam(name = "nomeUsuario") String nomeUsuario) {
+    	List<Usuario> usuario = usuarioRepository.pesquisarPorNome(nomeUsuario.trim().toUpperCase());
+    	return new ResponseEntity<List<Usuario>>(usuario, HttpStatus.OK);
+    }
+    
 }
--- src/main/java/br/com/springboot/crud/model/Usuario.java
+++ src/main/java/br/com/springboot/crud/model/Usuario.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
 @Entity
 public class Usuario implements Serializable {
@@ -14,7 +15,8 @@ public class Usuario implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_usuario")
+	@SequenceGenerator(name = "seq_usuario",sequenceName = "seq_usuario", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_usuario")
 	@Column(name = "id")
 	private Long id;
 
