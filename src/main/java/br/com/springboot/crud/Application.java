e1ba372a07496e1b2f5dadebc5a2f87061f98863
Larissa <larissastarosky@gmail.com>
2023-01-28 19:11:17 -0300

    remove comentário

--- src/main/java/br/com/springboot/crud/Application.java
+++ src/main/java/br/com/springboot/crud/Application.java
@@ -3,10 +3,6 @@ package br.com.springboot.crud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- *
- * Spring Boot application starter class
- */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
